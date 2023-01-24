bf0e65ab9613d984e4c6d3562e7385aef240ab69
MJ <max_jared@aol.com>
2023-01-24 14:19:51 +0100

    js ex 40 done

--- js/40/js/main.js
+++ js/40/js/main.js
@@ -1,19 +1,18 @@
-let fname = document.getElementById("name");
-let surname = document.getElementById("surname");
-let phone = document.getElementById("phone");
+function fill() {
 
+    let first_name = document.getElementById("first").value;
+    let last_name = document.getElementById("last").value;
+    let telephone = document.getElementById("tel").value;
 
-let first_name = document.getElementById("first").value;
-let last_name = document.getElementById("last").value;
-let telephone = document.getElementById("tel").value;
+    let fname = document.getElementById("name");
+    let surname = document.getElementById("surname");
+    let phone = document.getElementById("phone");
 
-
-function fill(a, b, c) {
-    fname.textContent = a;
-    surname.textContent = b;
-    phone.textContent = c;
+    fname.innerText = first_name;
+    surname.innerText = last_name;
+    phone.innerText = telephone;
 }
 
 let but = document.querySelector("button");
 
-but.onclick = fill(first_name, last_name, telephone);
+but.onclick = fill;
