ddd0965650097bbb353233c50541c5ffb4ecf042
MJ <max_jared@aol.com>
2023-01-24 11:32:55 +0100

    js ex 34

---
+++ js/34/main.js
@@ -0,0 +1,9 @@
+let one = "Uwielbiam JavaScript";
+let two = "Jestem świetnym programistą";
+
+function connect(a, b) {
+    let connection = a + " " + b;
+    return connection;
+}
+
+console.log(connect(one, two));
