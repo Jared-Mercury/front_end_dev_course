af0572603a8c60f95d706b8b3171d4e08b9c956f
MJ <max_jared@aol.com>
2023-01-24 11:23:31 +0100

    js ex 33

---
+++ js/33/js/main.js
@@ -0,0 +1,7 @@
+let data = document.getElementById("opis");
+
+let but = document.querySelector("button");
+
+but.onclick = function() {
+    data.textContent = "Font Awesome is the Internet's icon library and toolkit, used by millions of designers, developers, and content creators."
+}
