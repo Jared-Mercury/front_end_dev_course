b62c4e5aaf0d822d67cd36a94a3121e33706b5c4
MJ <max_jared@aol.com>
2023-03-28 13:31:40 +0200

    DOM workshop1

---
+++ DOM/js/script.js
@@ -0,0 +1,15 @@
+const setBackground = () => {
+
+    let p1 = document.querySelector('.first');
+    let p2 = document.querySelector('.last');
+
+    p1.classList.toggle('bg-red');
+    p2.classList.toggle('bg-yellow');
+
+
+    console.log("click");
+}
+
+let btnSetBackground = document.getElementById("set-background");
+
+btnSetBackground.addEventListener('click', setBackground);
