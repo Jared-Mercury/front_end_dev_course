e95218c5d3b859df69a5131aff513ab22060aae6
MJ <max_jared@aol.com>
2023-04-09 10:50:47 +0200

    jQuery_workshops_correction_3

--- jQuery/3/js/main.js
+++ jQuery/3/js/main.js
@@ -1,10 +1,15 @@
 let sum = 0;
+let x = 0;
 
 $("#count-sum").click(function() {
-    $(".salary").map(function() {
-        sum += Number(this.innerHTML);
+    if (x < 1) {
+        $(".salary").map(function() {
+            sum += Number(this.innerHTML);
 
-    });
+        });
+        x++;
+    }
     console.log(sum);
     $("#sum").html(sum);
+
 });
